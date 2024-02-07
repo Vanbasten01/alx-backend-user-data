b22f10264f23ba0562b017208e8ccd4fa38781d0
Vanbasten01 <fouad.yasin01@gmail.com>
2024-02-07 15:38:36 +0100

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,17 @@ import re
 import logging
 
 
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """a function called filter_datum that returns
+    the log message obfuscated """
+    for field in fields:
+        message = re.sub(f"{field}=.*?{separator}",
+                         f"{field}={redaction}{separator}",
+                         message)
+    return message
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
@@ -21,14 +32,3 @@ class RedactingFormatter(logging.Formatter):
         """Custom logging formatter redacting sensitive fields."""
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
-
-
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """a function called filter_datum that returns
-    the log message obfuscated """
-    for field in fields:
-        message = re.sub(f"{field}=.*?{separator}",
-                         f"{field}={redaction}{separator}",
-                         message)
-    return message
