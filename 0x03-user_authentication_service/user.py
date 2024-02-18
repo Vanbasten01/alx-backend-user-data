164b6f17498cc803075a2e3fb6e4894fd0e33371
Vanbasten01 <fouad.yasin01@gmail.com>
2024-02-18 09:29:10 +0100

    User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,10 +5,11 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class User(Base):
     """ User class """
     __tablename__ = "users"
-    
+
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
