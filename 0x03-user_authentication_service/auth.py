508f78518939e92ddba8c9c91ed628bc336a1205
Vanbasten01 <fouad.yasin01@gmail.com>
2024-02-18 09:44:29 +0100

    Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+""" auth Module """
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+import bcrypt
+import uuid
+
+
+def _hash_password(password: str) -> bytes:
+    """ a mothod for the password's hashing """
+    if not password or not isinstance(password, str):
+        return None
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+
+def _generate_uuid() -> str:
+    """Generate a new UUID string."""
+    return str(uuid.uuid4())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user with the given email and password."""
+        try:
+            existing_user = self._db.find_user_by(email=email)
+            if existing_user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email, password) -> bool:
+        """ a method that validates a password"""
+        try:
+            check_user = self._db.find_user_by(email=email)
+            if check_user:
+                return bcrypt.checkpw(password.encode('utf-8'),
+                                      check_user.hashed_password)
+        except NoResultFound:
+            return False
+
+    def create_session(self, email: str) -> str:
+        """Creates a new session for the user with the given
+             email and returns the session ID."""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return user.session_id
+        except NoResultFound:
+            return None
