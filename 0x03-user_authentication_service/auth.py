f15037d01bc833ae2889c1b3b13b596e0248d5d1
Vanbasten01 <fouad.yasin01@gmail.com>
2024-02-18 09:50:24 +0100

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -37,7 +37,7 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
 
-    def valid_login(self, email, password) -> bool:
+    def valid_login(self, email: str, password: str) -> bool:
         """ a method that validates a password"""
         try:
             check_user = self._db.find_user_by(email=email)
