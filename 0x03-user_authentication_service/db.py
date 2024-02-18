88b25786d1da49f14154105d6e986414a35c3dd2
Vanbasten01 <fouad.yasin01@gmail.com>
2024-02-18 09:30:31 +0100

    db

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email, hashed_password):
+        """A method to add a new user to the database"""
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
+
+    def find_user_by(self, **kwargs):
+        """ find a specific usic """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if not user:
+                raise NoResultFound
+            return user
+        except InvalidRequestError as e:
+            raise e
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ it updates a specific user"""
+        user = self.find_user_by(id=user_id)
+        if user:
+            try:
+                for key, val in kwargs.items():
+                    if hasattr(user, key):
+                        setattr(user, key, val)
+                    else:
+                        raise ValueError
+                self._session.commit()
+            except ValueError:
+                self._session.rollback()
